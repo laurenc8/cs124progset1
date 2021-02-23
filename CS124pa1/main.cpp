376474d6df22c47438462a22e644e1e181aead7c
laurenc8 <laurenc8822@gmail.com>
2021-02-23 03:14:21 -0500

    prim

--- CS124pa1/main.cpp
+++ CS124pa1/main.cpp
@@ -1,42 +1,96 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-//using adjmatrix = vector<vector<float>>; //use this?
+//using adjmatrix = vector<vector<float> >; //use this?
 //notes from zach: return graph(vector<float>(0, n)) to generate an empty graph
 
-vector<vector<float>> firstGraphGenerator(int n)
+vector<vector<float> > firstGraphGenerator(int n)
 {
     //generates the first type of graph
     srand((unsigned)time(NULL));
-    vector<vector<float>> graph = vector<vector<float>>(n, vector<float>(n, 0));
+    vector<vector<float> > graph = vector<vector<float> >(n, vector<float>(n, 0));
     for(int i = 0; i < n; ++i)
     {
         for(int j = 0; j < n; ++j)
         {
-            graph[i][j] = (double) rand()/RAND_MAX;
+            if(i < j)
+            {
+                graph[i][j] = (double) rand()/RAND_MAX;
+            }
+            else if (i == j)
+            {
+                graph[i][j] = 2;
+            }
+            else
+            {
+                graph[j][i] = graph[i][j];
+            }
         }
     }
     return graph;
 }
 
 
-vector<vector<float>> secondGraphGenerator(int n)
+vector<vector<float> > secondGraphGenerator(int n)
 {
     //generates the second type of graph
 }
 
 
-vector<vector<float>> thirdGraphGenerator(int n)
+vector<vector<float> > thirdGraphGenerator(int n)
 {
     //generates the third type of graph
 }
 
 
-void prim(const vector<vector<float>>& graph)
+void prim(const vector<vector<float> >& graph, int n)
 {
-    //prims algorithm
+    vector<float> dist = vector<float> (n, 0);
+    vector<float> prev = vector<float> (n, NULL);
+    vector<float> s = vector<float> (n, NULL);
+    vector<float> h = vector<float> (n, NULL);
+    h[0] = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        dist[i] = 2;
+    }
+    dist[0] = 0;
+    while(!h.empty())
+    {
+        int min = 2;
+        int v = 0;
+        for(int i = 0; i < n; ++i)
+        {
+            if(h[i] < min)
+            {
+                min = h[i];
+                v = i;
+            }
+        }
+        h[min] = NULL;
+        for(int j = 0; j < n; ++j)
+        {
+            if(s[j] != NULL)
+            {
+                s[j] = v;
+            }
+        }
+        for(int w = 0; w < n; ++w)
+        {
+            if(count(s.begin(), s.end(), (float)w) == 0)
+            {
+                if(dist[w] > graph[v][w])
+                {
+                    dist[w] = graph[v][w];
+                    prev[w] = v;
+                    h[w] = dist[w];
+                }
+            }
+        }
+    }
 }
 
 
@@ -61,8 +115,6 @@ int main()
 
 
     //perform mst algorithm
-    //prim(graph);
+    prim(graph, n);
     return 0;
 }
-
-
