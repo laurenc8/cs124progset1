4feace397278cfa290e51a9f611afa5f558fda72
Helena Jiang <helenajiang@helenas-mbp.lan>
2021-02-25 14:57:45 -0500

    has all 4 dimensions

--- CS124pa1/main.cpp
+++ CS124pa1/main.cpp
@@ -2,46 +2,6 @@
 #include <time.h>
 
 using namespace std;
-//using adjmatrix = vector<vector<float> >; //use this?
-//notes from zach: return graph(vector<float>(0, n)) to generate an empty graph
-
-// float** firstGraphGenerator(int n)
-// {
-//     //generates the first type of graph
-//     srand((unsigned)time(NULL));
-    
-//     for(int i = 0; i < n; ++i)
-//     {
-//         for(int j = 0; j < n; ++j)
-//         {
-//             if(i < j)
-//             {
-//                 graph[i][j] = (float) rand()/RAND_MAX;
-//             }
-//             else if (i == j)
-//             {
-//                 graph[i][j] = 2;
-//             }
-//             else
-//             {
-//                 graph[i][j] = graph[j][i];
-//             }
-//         }
-//     }
-//     return graph;
-// }
-
-
-// vector<vector<float> > secondGraphGenerator(int n)
-// {
-    //generates the second type of graph
-// }
-
-
-// vector<vector<float> > thirdGraphGenerator(int n)
-// {
-    //generates the third type of graph
-// }
 
 int extractmin(float dist[], int size, bool s[])
 {
@@ -122,6 +82,142 @@ float prim2d(int n)
     return size;
 }
 
+float prim3d(int n)
+{
+    srand((unsigned)time(NULL));
+    float graph[n][3];
+    for(int k = 0; k < n; ++k)
+    {
+        graph[k][0] = (float)rand()/RAND_MAX;
+        graph[k][1] = (float)rand()/RAND_MAX;
+        graph[k][2] = (float)rand()/RAND_MAX;
+    }
+    float size = 0;
+    float dist[n];
+    bool s[n];
+    for(int i = 0; i < n; ++i)
+    {
+        dist[i] = 2;
+        s[i] = false;
+    }
+    dist[0] = 0;
+    for(int j = 0; j < n; ++j)
+    {
+        int v = extractmin(dist, n, s);
+        size += dist[v];
+        s[v] = true;
+        for(int w = 0; w < n; ++w)
+        {
+            float edgeWeight = (float) sqrt((float)pow(graph[w][0] - graph[v][0], 2) +
+                    (float)pow(graph[w][1] - graph[v][1], 2) + (float)pow(graph[w][2] - graph[v][2], 2));
+            if((v != w) && (s[w] == false) && (dist[w] > edgeWeight))
+            {
+                dist[w] = edgeWeight;
+            }
+        }
+    }
+    return size;
+}
+
+float prim4d(int n)
+{
+    srand((unsigned)time(NULL));
+    float graph[n][4];
+    for(int k = 0; k < n; ++k)
+    {
+        graph[k][0] = (float)rand()/RAND_MAX;
+        graph[k][1] = (float)rand()/RAND_MAX;
+        graph[k][2] = (float)rand()/RAND_MAX;
+        graph[k][3] = (float)rand()/RAND_MAX;
+    }
+    float size = 0;
+    float dist[n];
+    bool s[n];
+    for(int i = 0; i < n; ++i)
+    {
+        dist[i] = 2;
+        s[i] = false;
+    }
+    dist[0] = 0;
+    for(int j = 0; j < n; ++j)
+    {
+        int v = extractmin(dist, n, s);
+        size += dist[v];
+        s[v] = true;
+        for(int w = 0; w < n; ++w)
+        {
+            float edgeWeight = (float) sqrt((float)pow(graph[w][0] - graph[v][0], 2)
+                    + (float)pow(graph[w][1] - graph[v][1], 2) + (float)pow(graph[w][2] - graph[v][2], 2)
+                                                                 + (float)pow(graph[w][3] - graph[v][3], 2));
+            if((v != w) && (s[w] == false) && (dist[w] > edgeWeight))
+            {
+                dist[w] = edgeWeight;
+            }
+        }
+    }
+    return size;
+}
+
+
+int main()
+{
+    //generate graph
+    int n;
+    cout << "Give a value of n: " << endl;
+    cin >> n;
+
+    //graph = secondGraphGenerator();
+    //graph = thirdGraphGenerator();
+
+    //perform mst algorithm
+    //cout << prim(n) << endl;
+    //cout << prim2d(n) << endl;
+    cout << prim3d(n) << endl;
+    cout << prim4d(n) << endl;
+    return 0;
+}
+
+//using adjmatrix = vector<vector<float> >; //use this?
+//notes from zach: return graph(vector<float>(0, n)) to generate an empty graph
+
+// float** firstGraphGenerator(int n)
+// {
+//     //generates the first type of graph
+//     srand((unsigned)time(NULL));
+
+//     for(int i = 0; i < n; ++i)
+//     {
+//         for(int j = 0; j < n; ++j)
+//         {
+//             if(i < j)
+//             {
+//                 graph[i][j] = (float) rand()/RAND_MAX;
+//             }
+//             else if (i == j)
+//             {
+//                 graph[i][j] = 2;
+//             }
+//             else
+//             {
+//                 graph[i][j] = graph[j][i];
+//             }
+//         }
+//     }
+//     return graph;
+// }
+
+
+// vector<vector<float> > secondGraphGenerator(int n)
+// {
+//generates the second type of graph
+// }
+
+
+// vector<vector<float> > thirdGraphGenerator(int n)
+// {
+//generates the third type of graph
+// }
+
 // float prim(float **graph, int n)
 // {
 //     float dist[n];
@@ -152,19 +248,3 @@ float prim2d(int n)
 //     }
 //     return size;
 // }
-
-int main()
-{
-    //generate graph
-    int n;
-    cout << "Give a value of n: " << endl;
-    cin >> n;
-
-    //graph = secondGraphGenerator();
-    //graph = thirdGraphGenerator();
-
-    //perform mst algorithm
-    cout << prim(n) << endl;
-    cout << prim2d(n) << endl;
-    return 0;
-}
